b0e4aaff4e199067b14d9a641dd3b60e1be87aed
CoRNeRNoTe <cornernote@gmail.com>
2025-04-13 13:52:07 +0930

    Update deploy-attack.js

--- deploy-attack.js
+++ deploy-attack.js
@@ -5,8 +5,8 @@ import { getAllServers, hackServer, getServerAvailableRam } from "./utils/server
  * @param {string[]} args - current arguments, not including "run script.js"
  * @returns {string[]} - the array of possible autocomplete options
  */
-export function autocomplete(data, flags) {
-	return data.servers;
+export function autocomplete(data, args) {
+    return args.length === 0 ? ["--purchased-only", "--hacked-only"] : data.servers;
 }
 
 /** @param {NS} ns */
@@ -20,9 +20,18 @@ export async function main(ns) {
     const scriptRam = ns.getScriptRam("attack.js");
     
     // Get all servers and filter out those we can't hack
+    const usePurchasedServersOnly = ns.args.includes("--purchased-only");
+    const useHackedServersOnly = ns.args.includes("--hacked-only");
     const allServers = getAllServers(ns);
     const hackableServers = allServers.filter(server => {
         const requiredHackingLevel = ns.getServerRequiredHackingLevel(server);
+
+        // Only use our own servers
+        if (usePurchasedServersOnly && !server.startsWith("pserv-") && server !== "home") return false;
+
+        // Only use hacked servers not our own
+        if (useHackedServersOnly && (server.startsWith("pserv-") || server === "home")) return false;
+
         return requiredHackingLevel <= ns.getHackingLevel();
     });
     
