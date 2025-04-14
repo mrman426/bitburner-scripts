c3797c28b482f1543dda6e164dc365b94d9aea81
CoRNeRNoTe <cornernote@gmail.com>
2025-04-14 19:32:27 +0930

    cleanup

--- show-targets.js
+++ show-targets.js
@@ -3,20 +3,16 @@ import { listView } from "./utils/console.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
-    // Analyze each server and calculate a score
-    const serverScores = getServerScores(ns, getAllServers(ns))
+    const formattedData = getServerScores(ns, getAllServers(ns))
         .sort((a, b) => b.score - a.score)
+        .map(s => ({
+            Server: s.server,
+            Score: s.score.toFixed(2),
+            Money: `$${ns.formatNumber(s.maxMoney)}`,
+            Security: s.minSecurity.toFixed(2),
+            'Time (s)': (s.timeToAttack / 1000).toFixed(2),
+            'Threads (w/g/h)': `${s.threads.weaken}/${s.threads.grow}/${s.threads.hack}`
+        }));
     
-    // Format the data for listView
-    const formattedData = serverScores.slice(0, 100).map(s => ({
-        Server: s.server,
-        Score: s.score.toFixed(2),
-        Money: `$${ns.formatNumber(s.maxMoney)}`,
-        Security: s.minSecurity.toFixed(2),
-        'Time (s)': (s.timeToAttack / 1000).toFixed(2),
-        'Threads (w/g/h)': `${s.threads.weaken}/${s.threads.grow}/${s.threads.hack}`
-    }));
-    
-    // Print header and formatted table
-    ns.tprint("=== Top 100 Best Servers to Attack ===\n" + listView(formattedData));
+    ns.tprint("=== Top Servers to Attack ===\n" + listView(formattedData));
 } 
