1dcff86d375e50d94180e1a5a713a75cde19f5c8
CoRNeRNoTe <cornernote@gmail.com>
2025-04-15 17:40:04 +0930

    totals and more info about ram

--- show-purchased-servers.js
+++ show-purchased-servers.js
@@ -1,5 +1,5 @@
 import { getAvailableRam } from "./utils/server.js";
-import { formatRam, formatMoney, listView } from "./utils/console.js";
+import { formatRam, formatMoney, formatPercent, listView, detailView } from "./utils/console.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
@@ -10,17 +10,37 @@ export async function main(ns) {
         return;
     }
 
+    let totalMaxRam = 0;
+    let totalUsedRam = 0;
+    let totalCost = 0;
+
     const serverData = purchasedServers
         .sort((a, b) => ns.getServerMaxRam(b) - ns.getServerMaxRam(a))
         .map(server => {
             const maxRam = ns.getServerMaxRam(server);
+            const availableRam = getAvailableRam(ns, server);
+            const usedRam = maxRam - availableRam;
+            const cost = ns.getPurchasedServerCost(maxRam);
+
+            totalMaxRam += maxRam;
+            totalUsedRam += usedRam;
+            totalCost += cost;
+
             return {
                 "Server Name": server,
                 "RAM": `${formatRam(ns, maxRam)}`,
-                "Free RAM": `${formatRam(ns, getAvailableRam(ns, server))}`,
-                "Cost": `${formatMoney(ns, ns.getPurchasedServerCost(maxRam))}`
+                "RAM Usage": `${formatPercent(ns, 1 - (availableRam / maxRam))}`,
+                "Cost": `${formatMoney(ns, cost)}`
             };
         });
-    
-    ns.tprint("\n=== Purchased Servers ===\n" + listView(serverData));
-} 
+
+    const totals = {
+        "Total RAM": formatRam(ns, totalMaxRam),
+        "Total Used RAM": formatRam(ns, totalUsedRam),
+        "Total Free RAM": formatRam(ns, totalMaxRam - totalUsedRam),
+        "Total RAM Usage": formatPercent(ns, totalUsedRam / totalMaxRam),
+        "Total Cost": formatMoney(ns, totalCost)
+    };
+
+    ns.tprint("\n=== Purchased Servers ===\n" + listView(serverData) + detailView(totals));
+}
