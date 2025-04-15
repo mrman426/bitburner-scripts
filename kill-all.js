1d0052bd6bceff8d56e30936fd8f0118950a4d61
CoRNeRNoTe <cornernote@gmail.com>
2025-04-15 15:53:22 +0930

    Update kill-all.js

--- kill-all.js
+++ kill-all.js
@@ -1,24 +1,14 @@
+import { getAllServers } from "utils/server.js";
+
 /** @param {NS} ns */
 export async function main(ns) {
-    const visited = new Set();
-    
-    // Recursive function to scan all servers
-    function scanAll(server) {
-        if (visited.has(server)) return;
-        visited.add(server);
-        
-        // Kill all scripts on current server
+    const servers = getAllServers(ns);
+    const otherServers = servers.filter(server => server !== "home");
+
+    for (const server of otherServers) {
         ns.killall(server);
-        
-        // Scan connected servers
-        const connected = ns.scan(server);
-        for (const s of connected) {
-            scanAll(s);
-        }
     }
-    
-    // Start scanning from home
-    scanAll('home');
-    
+
+    ns.killall("home");
     ns.tprint('All scripts have been killed on all servers.');
-} 
+}
