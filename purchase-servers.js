996fa81d7ad9490affc05791f43d98e0ca6deec9
CoRNeRNoTe <cornernote@gmail.com>
2025-04-19 01:35:42 +0930

    Update purchase-servers.js

--- purchase-servers.js
+++ purchase-servers.js
@@ -73,7 +73,7 @@ export async function main(ns) {
 
         // Don't buy servers with less RAM than the owned server with the most RAM
         if (maxAffordableRam < maxOwnedRam && maxAffordableRam < maxGameRam) {
-            log(ns, `WARNINIG: Not purchasing a server with ${formatRam(ns, maxAffordableRam)} RAM for ${formatMoney(ns, maxAffordableCost)} as it is less than the maximum owned server RAM (${formatRam(ns, maxOwnedRam)}).`, verbose);
+            log(ns, `WARNING: Not purchasing a server with ${formatRam(ns, maxAffordableRam)} RAM for ${formatMoney(ns, maxAffordableCost)} as it is less than the maximum owned server RAM (${formatRam(ns, maxOwnedRam)}).`, verbose);
 
             if (loop) {
                 log(ns, `INFO: Checking again in ${sleepTime / 1000} seconds...`, verbose)
